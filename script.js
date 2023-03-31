663032678b0c2a36bb8a568a17f658e37a3553f7
Karthikvattekkatt <karthikvattekkatt@gmail.com>
2023-03-31 10:47:10 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+// console.log("Hello World");
+// let i = 10;
+// console.log(i);
+
+//Print numbers 0 to 10
+// for(let i=0;i<=10;i++){
+//     console.log(i);
+// }
+
+//Print numbers 10 to 0
+// for(let j=10;j>=0;j--)
+// {
+//     console.log(j);
+// }
+
+//Print sum of first 10 natural numbers
+//1+2+3...+10
+
+// let sum = 0;
+// for(let i=1;i<=10;i++){
+//     sum=sum+i;
+// }
+// console.log("Sum of first 10 natural numbers is ",sum);
+
+
+//while loop
+
+// let i =0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+// let i=2;
+// do{
+//     console.log(i);
+// }while(i>2)
+
+//for...of loop
+let students = ['Hari','Ram','sita']
+for(let stud of students){
+    console.log(`Hello ${stud}`);
+}
+
+//backtick ``
